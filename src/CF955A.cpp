34f5d0388136cee8513b21cb78178222199d5940
Jacob Ho <clh161@gmail.com>
2018-04-03 02:22:56 +0800

    Add CF955A.cpp

---
+++ src/CF955A.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <cmath>
+
+
+using namespace std;
+
+double solve(int h, int m, int hunger, int increaseRate, int cost, int decreaseRate);
+
+int main() {
+    int h, m, hunger, increaseRate, cost, decreaseRate;
+    cin >> h >> m >> hunger >> increaseRate >> cost >> decreaseRate;
+    printf("%.4f", solve(h, m, hunger, increaseRate, cost, decreaseRate));
+    return 0;
+}
+
+double solve(int h, int m, int hunger, int increaseRate, int cost, int decreaseRate) {
+    double cost1 = ceil((max(1200 - h * 60 - m, 0) * increaseRate + hunger) / decreaseRate) * cost * 0.8;
+    double cost2 = ceil(hunger / decreaseRate) * cost;
+    return min(cost1, cost2);
+}
